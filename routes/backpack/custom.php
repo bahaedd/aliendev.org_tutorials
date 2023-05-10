dd54f00ce26c4c4d0848ab1ebd86e8b74e05282c
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-05-10 12:34:36 +0100

    laravel-crud-packge6

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -21,4 +21,5 @@ Route::group([
     Route::crud('category', 'CategoryCrudController');
     Route::crud('tag', 'TagCrudController');
     Route::crud('article', 'ArticleCrudController');
+    Route::crud('image', 'ImageCrudController');
 }); // this should be the absolute last line of this file
