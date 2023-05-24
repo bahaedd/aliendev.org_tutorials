1945c9ffe7eab9f3951ef5b0c48a2a97e51a9b95
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-05-24 20:25:58 +0100

    laravel-vue-crud2

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,7 +1,7 @@
-const Welcome = () => import('./components/Welcome.vue' /* webpackChunkName: "resource/js/components/welcome" */)
-const GenreList = () => import('./components/genre/List.vue' /* webpackChunkName: "resource/js/components/genre/list" */)
-const GenreCreate = () => import('./components/genre/Add.vue' /* webpackChunkName: "resource/js/components/genre/add" */)
-const GenreEdit = () => import('./components/genre/Edit.vue' /* webpackChunkName: "resource/js/components/genre/edit" */)
+const Welcome = () => import('./Welcome.vue' /* webpackChunkName: "resource/js/welcome" */)
+const GenreList = () => import('./genre/List.vue' /* webpackChunkName: "resource/js/genre/list" */)
+const GenreCreate = () => import('./genre/Add.vue' /* webpackChunkName: "resource/js/genre/add" */)
+const GenreEdit = () => import('./genre/Edit.vue' /* webpackChunkName: "resource/js/genre/edit" */)
 
 export const routes = [
     {
